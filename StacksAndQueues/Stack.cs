eb9630f2c71f15ff15b390f9067f4d2c6ddc98bd
Amauri Lopez <lopeza16@tcnj.edu>
2018-06-15 20:01:52 -0400

    Added on to Stack.cs class

--- StacksAndQueues/Stack.cs
+++ StacksAndQueues/Stack.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Diagnostics;
 
 public class Stack
@@ -23,28 +22,82 @@ public class Stack
 
     //Start Stack class
     private Node top;
-    public Stack() => this.top = null;
+    private int _size;
+
+    public Stack()
+    {
+        this.top = null;
+        this._size = 0;
+    }
+
+    public int GetSize()
+    {
+        return this._size;
+    }
 
     public void Push(object value)
     {
         Node temp = new Node(value, this.top);
         this.top = temp;
+        this._size++;
     }
 
     public object Pop()
     {
-        object value = this.top.Data;
-        this.top = this.top.Next;
+        object value = null;
+        if (!this.IsEmpty())
+        {
+            value = this.top.Data;
+            this.top = this.top.Next;
+        }
+        else { Debug.WriteLine("Nothing to pop."); }
 
+        this._size--;
         return value;
     }
 
-    static void Main(string []args)
+    public bool IsEmpty()
     {
-        
+        return this._size == 0;
     }
 
+    public object Peek()
+    {
+        
+        if (!this.IsEmpty())
+            return this.top.Data;
+        else
+        {
+            Debug.WriteLine("Nothing to peek.");
+            return null;
+        }
+    }
 
+    public void Clear()
+    {
+        //Lose the reference to the top node
+        this.top = null;
+        this._size = 0;
+    }
 
+}
 
+//This is just a test program
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Stack myStack = new Stack();
+        myStack.Push(5);
+        Debug.WriteLine(myStack.Peek());
+
+        myStack.Push(6);
+        Debug.WriteLine(myStack.Peek());
+
+        object value = myStack.Pop();
+        Debug.WriteLine(value + " was popped.");
+        Debug.WriteLine(myStack.Peek());
+        
+        
+    }
 }
