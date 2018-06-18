686fed059256e12c0f4d233e1bfe3d798a576c7b
Amauri Lopez <lopeza16@tcnj.edu>
2018-06-18 00:25:57 -0400

    Rehauled Architecture for decoupling

---
+++ MyDSLibraries/DSProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using DSInterfaces;
+using DSFactory;
+
+namespace DSProgram
+{
+    class Program {
+
+        static void Main(string[] args)
+        {
+            #region Stacks
+            StackFactory stackFactory = new StackFactory();
+
+            IStack myStack = stackFactory.CreateStack();
+
+            myStack.Push(5);
+            Debug.WriteLine(myStack.Pop());
+            myStack.Push(4);
+            myStack.Push(15);
+            Debug.WriteLine(myStack.Peek());
+            myStack.Pop();
+            Debug.WriteLine(myStack.Pop());
+            Debug.WriteLine(myStack.GetSize());
+
+            #endregion
+
+        }
+    }
+}
